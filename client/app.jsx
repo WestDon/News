c8042419f258909d57e8b4fc116a454ab4e54bfa
Andrew <andrei.vaschuk94@gmail.com>
2026-07-03 13:39:26 +0300

    Add SportNews route to app.jsx

--- client/app.jsx
+++ client/app.jsx
@@ -9,6 +9,7 @@ import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import './styles/main.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Home from './views/Home/Home';
+import SportNews from './views/SportNews';
 
 var store = redux.createStore(reducer);
 
@@ -20,9 +21,11 @@ ReactDOM.render(
         <Route exact path="/" component={Home} />
         <Route exact path="/Home" component={Home} />
         <Route exact path="/News" component={NewsView} />
+        <Route exact path="/SportNews" component={SportNews} />
       </Switch>
     </Provider>
   </Router>,
 
   document.getElementById("app")
 );
+```
